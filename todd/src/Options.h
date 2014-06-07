0ec776bb60cf4826996609d919023a35cf6a5f53
Mariusz Pilipczuk <projekt.darma@gmail.com>
2014-06-08 00:28:51 +0100

    Commit for snapshot #6

--- todd/src/Options.h
+++ todd/src/Options.h
@@ -42,21 +42,59 @@
 
 using namespace std;
 
+/**
+ * \brief A class that handles options.
+ */
 class Options
 {
 private:
 	string getPathToOptions();
 
 public:
+	/**
+	 * \brief The key used to confirm stuff (default is X).
+	 */
 	SDL_Keycode confirmKey;
+
+	/**
+	 * \brief The key used to cancel stuff (default is Z).
+	 */
 	SDL_Keycode cancelKey;
+
+	/**
+	 * \brief The key for walking up (default is up arrow key).
+	 */
 	SDL_Keycode upKey;
+
+	/**
+	 * \brief The key for walking down (default is down arrow key).
+	 */
 	SDL_Keycode downKey;
+
+	/**
+	 * \brief The key for walking left (default is left arrow key).
+	 */
 	SDL_Keycode leftKey;
+
+	/**
+	 * \brief The key for walking right (default is right arrow key).
+	 */
 	SDL_Keycode rightKey;
+
+	/**
+	 * \brief The key for opening the menu (default is ESC).
+	 */
 	SDL_Keycode menuKey;
 
 	Options();
+
+	/**
+	 * \brief Save the current options in the appropriate file.
+	 *
+	 * The file to which settings are saved and read from is platform-dependent.
+	 * It is called 'madd.todd-options', and on Windows it is stored in APPDATA, and on UNIX it's
+	 * stored in HOME.
+	 */
 	void save();
 };
 

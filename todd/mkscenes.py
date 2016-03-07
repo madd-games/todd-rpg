7a1fcecdf160a67364fd1a6aa2d3b45d224591dd
madd-games <madd.gods@gmail.com>
2016-03-07 14:40:01 +0000

    Made mkscenes.py work on windows

--- todd/mkscenes.py
+++ todd/mkscenes.py
@@ -7,4 +7,7 @@ for name in os.listdir("%s/maps" % srcdir):
 	if name.endswith(".tmx"):
 		mapname = name[:-4]
 		print ">Make map %s" % mapname
-		os.system("python \"%s/convertmap.py\" \"%s/maps/%s.tmx\" \"%s/data/scenes/%s.scn\"" % (srcdir, srcdir, mapname, srcdir, mapname))
+		if os.platform.startswith("win"):
+			os.system("\"%s\\convertmap.py\" \"%s/maps/%s.tmx\" \"%s/data/scenes/%s.scn\"" % (srcdir, srcdir, mapname, srcdir, mapname))
+		else:
+			os.system("python \"%s/convertmap.py\" \"%s/maps/%s.tmx\" \"%s/data/scenes/%s.scn\"" % (srcdir, srcdir, mapname, srcdir, mapname))

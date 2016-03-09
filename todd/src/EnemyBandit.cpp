effa8a400f729d26c56196042dcc102ca432f258
NovaCookie232 <ddcaws@gmail.com>
2016-03-09 10:40:51 +0000

    Balancing AGI and ACC stats

--- todd/src/EnemyBandit.cpp
+++ todd/src/EnemyBandit.cpp
@@ -56,6 +56,7 @@ EnemyBandit::EnemyBandit() : numPotions(5), numFireSlash(3)
 	stats.STR = 15;
 	stats.DEF = 10;
 	stats.ACC = 80;
+	stats.AGI = 10;
 
 	memset(resist, 0, sizeof(int)*Element::NUM_ELEMENTS);
 	resist[Element::FIRE] = -5;
